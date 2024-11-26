90a503dae8a6426ab1aae00c5d81128687c072ef
prerana <prerana.chaks@gmail.com>
2024-11-26 11:21:26 +0800

    adding new arg

--- src/coreml/infer_from_quantized_model.sh
+++ src/coreml/infer_from_quantized_model.sh
@@ -1,1 +1,1 @@
-python infer.py -prompt "A dog is running on highway." -i ml_package_sdxl -o results --model-version stabilityai/stable-diffusion-xl-base-1.0 
+python infer.py -prompt "A dog is running on highway." -i ml_package_sdxl -o results --model-version stabilityai/stable-diffusion-xl-base-1.0 --image2image
