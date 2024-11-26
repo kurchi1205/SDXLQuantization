102b2733f293cc6108f4d12612f26c86bb885666
prerana <prerana.chaks@gmail.com>
2024-11-26 12:31:42 +0800

    xl version conversion

--- src/coreml/convert_sdxl_to_ml_package.sh
+++ src/coreml/convert_sdxl_to_ml_package.sh
@@ -1,1 +1,1 @@
-python torch2coreml.py --model-version stabilityai/stable-diffusion-xl-base-1.0 --convert-unet --convert-text-encoder --convert-vae-decoder -o ml_package_sdxl
+python torch2coreml.py --model-version stabilityai/stable-diffusion-xl-base-1.0 --convert-unet --convert-text-encoder --convert-vae-decoder -o ml_package_sdxl --xl-version
